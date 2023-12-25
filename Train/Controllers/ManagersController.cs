18eaba406a34a5f7e8175e38584a1e7855eb8eec
Alshukaili988 <manal.hilal77@gmail.com>
2023-12-25 11:21:45 +0400

    Create NewEmployee

---
+++ Train/Controllers/ManagersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Train.Data;
+using Train.Models;
+using Train.Services;
+
+namespace Train.Controllers
+{
+    public class ManagersController : Controller
+    {
+
+   
+        private readonly ApplicationDbContext _context;
+        public ManagersController(ApplicationDbContext context)
+        {
+            _context = context;
+            
+        }
+        [HttpGet("/managers")]
+        public IActionResult Index()
+        {
+            var managers = _context.Managers.ToList();
+            return View(managers);
+
+            
+        }
+        [HttpPost]
+        public IActionResult Create(Managers manager)
+        {
+            // logic
+
+            // save to database
+            _context.Managers.Add(manager);
+            _context.SaveChanges();
+            // return to list of employees
+            return RedirectToAction("Index");
+        }
+    }
+}
